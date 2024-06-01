c4fb8ae41aea0efb59037a4eed13d14121f189ef
mariful-nyx <mariful@heliosinsider.com>
2024-06-01 13:58:23 +0600

    login page design for nxtbn dashboard

---
+++ src/components/login/LoginRightSide.tsx
@@ -0,0 +1,57 @@
+import illustration from "../../assets/Illustration.png";
+import product from "../../assets/Product 1.png";
+import Slider from "./Slides";
+
+function LoginRightSide() {
+  const slides = [
+    {
+      content: (
+        <div className="">
+          <div className="">
+            <div className="text-secondary-50 w-[360px] text-center">
+              <h2 className="text-[30px] font-nunito-h3 mb-6 md:text-[30px] short:text-[24px]">
+                Single-Click Payment Gateway Installation – No Jokes!
+              </h2>
+            </div>
+            <div className="text-primary-100 text-center w-[360px]">
+              <p className="text-[14px] md:text-12px">
+                Description: Instantly install thousand+ major payment gateways
+                such as Stripe, 2Checkout, Square, PayPal, and Adyen, along with
+                thousand others, with our single-click solution. Simplify your
+                checkout process for smoother transactions, or customize your
+                own payment gateway to fit your needs
+              </p>
+            </div>
+          </div>
+        </div>
+      ),
+    },
+    {
+      content: <div>Slide 2</div>,
+    },
+    {
+      content: <div>Slide 3</div>,
+    },
+  ];
+  
+  return (
+    <div className="bg-[#277200] h-screen relative grid gap-5">
+        <img
+          src={illustration}
+          alt=""
+          className="absolute h-[45%] top-[9%] right-[50%] translate-x-[50%] w-[400px] lg:w-[450px] xl:w-[500px] md:w-[400px] object-contain "
+        />
+
+        <img
+          src={product}
+          alt=""
+          className="absolute h-[30%] top-[21%] xl:w-[340px] lg:right-[25%] lg:translate-x-[25%] lg:w-[300px] md:right-[20%] md:translate-x-[20%] md:w-[280px] w-[280px] right-[18%] translate-x-[18%] object-contain"
+
+        />
+
+      <Slider slides={slides} interval={10000} />
+    </div>
+  );
+}
+
+export default LoginRightSide;
---
+++ src/components/login/Slides.tsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+
+interface Slide {
+  content: JSX.Element;
+}
+
+interface SliderProps {
+  slides: Slide[];
+  interval?: number;
+}
+
+const Slider: React.FC<SliderProps> = ({ slides, interval = 3000 }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
+    }, interval);
+
+    return () => clearInterval(timer);
+  }, [slides.length, interval]);
+
+  return (
+    <div className="w-full h-full overflow-hidden relative">
+      <div className="absolute bottom-[6%] right-1/2 w-full translate-x-1/2">
+        <div
+          className="flex mt-[60%] transition-transform duration-700"
+          style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+        >
+          {slides.map((slide, index) => (
+            <div
+              key={index}
+              className="w-full flex-shrink-0 flex items-center justify-center"
+            >
+              {slide.content}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="absolute bottom-[2%] left-0 right-0 flex justify-center mb-2">
+        {slides.map((_, index) => (
+          <div
+            key={index}
+            className={`h-2 w-2 rounded-full mx-1 cursor-pointer ${
+              currentIndex === index ? "bg-base-100" : "bg-base-200"
+            }`}
+            onClick={() => setCurrentIndex(index)}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Slider;

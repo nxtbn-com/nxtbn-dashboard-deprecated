df0eefb944638414b3cfad9bcec8088a97e6fe85
mariful-nyx <mariful@heliosinsider.com>
2024-06-10 16:45:46 +0600

    jwt authentication implement for nxtbn dashboard login

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,62 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import { AddNewProduct, Customers, Dashboard, OrderDetails, Orders, Products, Login } from "./pages";
+import {
+  AddNewProduct,
+  Customers,
+  Dashboard,
+  OrderDetails,
+  Orders,
+  Products,
+  Login,
+} from "./pages";
 import RootLayout from "./layouts/RootLayout";
+
+
 function App() {
+  const accessToken = localStorage.getItem("accessToken");
+  const refreshToken = localStorage.getItem("refreshToken");
+  
+
   return (
     <BrowserRouter>
-        <Routes>
-            {/* 
+      <Routes>
+        {/* 
               Redirect root path to /login.
               This ensures that if a user navigates to the root URL, they are redirected to the login page.
               This setup avoids conflicts with Django URLs by considering all dashboard-related routes under /dashboard.
               It provides the flexibility to serve the dashboard via Django URLs and views or as an independent host/port.
             */}
-            <Route path="/" element={<Navigate to="dashboard/login" replace />} />
+        
 
-            {/* 
+        {/* 
               Dashboard routes that require RootLayout.
               These routes are nested under /dashboard to avoid conflicts with Django and to ensure a consistent layout.
             */}
-            <Route path="/dashboard" element={<RootLayout />}>
-                <Route index element={<Dashboard />} />
-                <Route path="orders" element={<Orders />} />
-                <Route path="orders/:id" element={<OrderDetails />} />
-                <Route path="products" element={<Products />} />
-                <Route path="products/add-new-product" element={<AddNewProduct />} />
-                <Route path="customers" element={<Customers />} />
-            </Route>
+        {accessToken && refreshToken ? (
+          <Route path="/dashboard" element={<RootLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="orders" element={<Orders />} />
+            <Route path="orders/:id" element={<OrderDetails />} />
+            <Route path="products" element={<Products />} />
+            <Route
+              path="products/add-new-product"
+              element={<AddNewProduct />}
+            />
+            <Route path="customers" element={<Customers />} />
+            
+          </Route>
+        ) : (
+          <>
+          <Route path="/" element={<Navigate to="dashboard/login" replace />} />
+          <Route path="*" element={<Navigate to="/dashboard/login" />} />
+          </>
+        )}
 
-            {/* 
+        {/* 
               Login route.
               This route is outside the RootLayout to provide a distinct layout for the login page.
             */}
-            <Route path="dashboard/login" element={<Login />} />
-        </Routes>
+        <Route path="dashboard/login" element={<Login />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/components/login/LoginLeftSide.tsx
+++ src/components/login/LoginLeftSide.tsx
@@ -1,43 +1,119 @@
+import axios from "axios";
 import nxtbnlogo from "../../assets/nxtbn_black.png";
-import { useEffect, useState } from "react";
+import { useEffect, useState, FormEvent } from "react";
+import { useNavigate } from "react-router-dom";
+
+
 
 function LoginLeftSide() {
-  // password and email validation
-  const validateEmail = (email: string): boolean => {
-    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return re.test(email);
-  };
 
-  const validatePassword = (password: string): boolean => {
-    // Password validation logic (e.g., min length 8)
-    return password.length >= 8;
-  };
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  interface UserState {
+    email: string;
+    password: string;
+  }
+  
+  const [user, setUser] = useState<UserState>({
+    email: '',
+    password: ''
+  });
+
   const [isEmailValid, setIsEmailValid] = useState(false);
   const [isPasswordValid, setIsPasswordValid] = useState(false);
   const [isFormValid, setIsFormValid] = useState(false);
 
+
+  // password and email validation
+  const validateEmail = (email: string): boolean => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(user.email);
+  };
+
+  const validatePassword = (password: string): boolean => {
+    // Password validation logic (e.g., min length 8)
+    return user.password.length >= 4;
+  };
+
+  
   useEffect(() => {
-    setIsEmailValid(validateEmail(email));
-  }, [email]);
+    setIsEmailValid(validateEmail(user.email));
+  }, [user.email]);
 
   useEffect(() => {
-    setIsPasswordValid(validatePassword(password));
-  }, [password]);
+    setIsPasswordValid(validatePassword(user.password));
+  }, [user.password]);
 
   useEffect(() => {
     setIsFormValid(isEmailValid && isPasswordValid);
   }, [isEmailValid, isPasswordValid]);
 
+
+  const updateUser = (updates: Partial<UserState>) => {
+    setUser(prevUser => ({
+      ...prevUser,
+      ...updates
+    }));
+  };
+
+
+
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
+    updateUser({email: e.target.value});
   };
 
   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
+    updateUser({password: e.target.value});
   };
+
+  
+  interface ResponseData {
+    user: {
+      id: number,
+      username: string,
+    },
+    token: {
+      access: string,
+      refresh: string
+    }
+  }
+
+
+  const [error, setError] = useState('');
+
+  const navigate = useNavigate()
+
+  const API_URL =  'http://127.0.0.1:8000' || 'http://localhost:8000'
+
+
+  const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post<ResponseData>(`${API_URL}/user/dashboard/api/login/`, {'email': user.email, 'password': user.password});
+
+
+      if (res.status === 200){
+        localStorage.setItem('username', res.data.user.username)
+        localStorage.setItem('accessToken', res.data.token.access)
+        localStorage.setItem('refreshToken', res.data.token.refresh)
+
+        navigate('/dashboard')
+        window.location.reload()
+
+      }  
+      
+
+    } catch (error:any) {
+      console.log(error)
+      if (error.response.status === 400){
+        setError(error.response.data.detail)
+      }
+      if (error.response.status === 500){
+        setError("Server Error")
+      }
+    }
+  };
+
+
 
   return (
     <div className="flex flex-col md:gap-[70px] gap-[150px] px-3 mt-[35px] md:mt-[80px] md:px-[5%] lg:px-[15%] xl:px-[20%]">
@@ -48,11 +124,12 @@ function LoginLeftSide() {
       <div className="flex flex-col justify-center mb-[150px] md:justify-start w-[100%] px-3 gap-10">
         <h3 className="text-[32px] font-nunito-h3 text-center md:text-start">Login to your account</h3>
         <div className="flex flex-col gap-10">
-          <form className="flex flex-col gap-6">
+          <p className="text-red-500 text-center">{error}</p>
+          <form className="flex flex-col gap-6" onSubmit={handleLogin}>
             <input
               type="email"
               name="email"
-              value={email}
+              value={user.email}
               onChange={handleEmailChange}
               placeholder="Email"
               className="w-full bg-secondary-50 outline-none border-none px-5 py-2 rounded-md"
@@ -60,7 +137,7 @@ function LoginLeftSide() {
             <input
               type="password"
               name="password"
-              value={password}
+              value={user.password}
               onChange={handlePasswordChange}
               placeholder="Password"
               className="w-full bg-secondary-50 outline-none border-none px-5 py-2 rounded-md"
@@ -84,6 +161,7 @@ function LoginLeftSide() {
                   : "bg-[#86D7B0] cursor-not-allowed"
               }`}
               disabled={!isFormValid}
+              type="submit"
             >
               Sign in with email
             </button>

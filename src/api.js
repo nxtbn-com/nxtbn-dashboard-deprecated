5fc8389f5a6196ff7159a85aba69b5ae6490046c
MySecondLanguage <mmamunism@gmail.com>
2024-06-11 22:58:58 +0600

    added refresh token

--- src/api.js
+++ src/api.js
@@ -5,6 +5,7 @@ const useApi = () => {
    
     const api = {
         adminLogin: (data, params={}) => axios.post(`user/dashboard/api/login/`, data, {params : params}),
+        refreshToken: (data, params={}) => axios.post(`user/dashboard/api/token/refresh/`, data, {params : params}),
         adminLogout: (data, params={}) => axios.post(`user/dashboard/api/logout/`, data, {params : params}),
     };
 
